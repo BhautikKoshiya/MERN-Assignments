dc2626a6aafc4f295375e7607b08b82db6cae139
Bhautik Koshiya <bhautik.koshiya@dal.ca>
2024-03-28 08:37:31 +0530

    code added

--- A3/app.js
+++ A3/app.js
@@ -5,7 +5,6 @@ const { registerRoutes } = require('./routes');
 const cors = require('cors');
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
